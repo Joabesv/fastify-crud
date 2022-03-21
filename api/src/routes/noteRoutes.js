6101df9cf493e38d9c820426d04d8cfc162b0fc6
Joabesv <joabevarjao123@gmail.com>
2022-03-21 17:09:27 -0300

    adding the controller

--- api/src/routes/noteRoutes.js
+++ api/src/routes/noteRoutes.js
@@ -1,16 +1,20 @@
+import { NotesController } from '../controllers/notesController.js';
+
+const notesController = NotesController();
+
 export function noteRoutes(app) {
   // create the note
-  app.post('/api/notes', (request, reply) => {});
+  app.post('/api/notes', notesController.createNote);
 
   // get the list of notes
-  app.get('/api/notes', (request, reply) => {});
+  app.get('/api/notes', notesController.getNotes);
 
   // get a single note
-  app.get('/api/notes/:id', (request, reply) => {});
+  app.get('/api/notes/:id', notesController.getNote);
 
   // update a note
-  app.put('/api/notes/:id', (request, reply) => {});
+  app.put('/api/notes/:id', notesController.updateNote);
 
   // delete a note
-  app.delete('/api/notes/:id', (request, reply) => {});
+  app.delete('/api/notes/:id', notesController.deleteNote);
 }
